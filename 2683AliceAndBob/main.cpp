868051b9a6e1ede61ec191c5610d9f4431cce71d
Piexubn <1442559529@qq.com>
2019-03-24 16:12:29 +0800

    2019/3/24 exercise

---
+++ 2683AliceAndBob/main.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  2683AliceAndBob
+//
+//  Created by 许滨楠 on 2019/3/24.
+//  Copyright © 2019 许滨楠. All rights reserved.
+//
+
+#include <iostream>
+int main(){int n;while(std::cin>>n&&n)std::cout<<(n%2?"Bob":"Alice") <<std::endl;return 0;}

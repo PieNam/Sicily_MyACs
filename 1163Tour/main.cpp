fc4a6be980b0a20abdfb83c67b4ccbdbe99c24f1
Piexubn <1442559529@qq.com>
2019-03-31 12:46:55 +0800

    2019/3/31 exercise

--- 1163Tour/main.cpp
+++ 1163Tour/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int i, j, n, x[51], y[51];
+    double dp[51][51], d[51][51];
+    while (cin >> n) {
+        for (i = 0; i < n; ++i) cin >> x[i] >> y[i];
+        for (i = 0; i < n; ++i)
+            for (j = 0; j < i; ++j)
+                d[i][j] = sqrt((x[i]-x[j])*(x[i]-x[j])
+                               +(y[i]-y[j])*(y[i]-y[j]));
+        dp[1][0] = dp[0][1] = d[1][0];
+        for (i = 2; i < n; ++i) {
+            dp[i][i-1] = 1<<30;
+            for (j = 0; j < i-1; ++j) {
+                dp[i][i-1] = min(dp[i][i-1], d[i][j] + dp[i-1][j]);
+                dp[i][j] = d[i][i-1] + dp[i-1][j];
+            }
+        }
+        double res = 1<<30;
+        for (i = 0; i < n - 1; ++i)
+            res = min(res, dp[n-1][i] + d[n-1][i]);
+        cout << fixed << setprecision(2) << res << endl;
+    }
     return 0;
 }

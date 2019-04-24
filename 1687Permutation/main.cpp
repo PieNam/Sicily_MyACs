e678762aee1b1930509b6e82b8d8be8b7e4e613f
Piexubn <1442559529@qq.com>
2019-04-24 09:01:01 +0800

    2019/4/24 exercise

--- 1687Permutation/main.cpp
+++ 1687Permutation/main.cpp
@@ -14,9 +14,9 @@ int main() {
     int m, n, i, j, dp[101][101] = {0};
     for (i = 1; i < 101; ++i) {
         dp[i][0] = 1;
-        for (j = 1; j < 101; ++j)
-            if (j == i - 1) dp[i][j] = 1;
-            else dp[i][j] = (((j+1)*dp[i-1][j])%2007 + (i-j)*dp[i-1][j-1]%2007)%2007;
+        dp[i][i-1] = 1;
+        for (j = 1; j < i-1; ++j)
+            dp[i][j] = (((j+1)*dp[i-1][j])%2007 + (i-j)*dp[i-1][j-1]%2007)%2007;
     }
     while (cin >> m >> n)
         cout << dp[m][n] << endl;

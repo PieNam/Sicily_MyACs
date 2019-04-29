a0ac70b7a5924aa27f8f330e350d5a432b276ac5
Piexubn <1442559529@qq.com>
2019-04-29 10:21:59 +0800

    2019/4/29 exercise

---
+++ 1087AFunnyGame/main.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  1087AFunnyGame
+//
+//  Created by 许滨楠 on 2019/4/29.
+//  Copyright © 2019 许滨楠. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;int main(){int n;while(cin>>n&&n)cout<<(n>2?"Bob\n":"Alice\n");}

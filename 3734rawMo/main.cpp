3cebf6a3d3781c5115b8771714c728f3c2a635bf
Piexubn <1442559529@qq.com>
2019-05-01 12:34:28 +0800

    2019/5/1 exercise

---
+++ 3734rawMo/main.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  3734rawMo
+//
+//  Created by 许滨楠 on 2019/5/1.
+//  Copyright © 2019 许滨楠. All rights reserved.
+//
+
+#include <iostream>
+int main(){int a,b;std::cin>>a>>b;std::cout<<a*b;return 0;}
